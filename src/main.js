22162f78ef8ec1d71ce1e14f82a798a0afc0a5c4
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2022-12-14 18:59:30 +0200

    tasks

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.config.productionTip = false
+
+Vue.use(Toasted,{
+    duration:1500,
+})
 
 createApp(App).use(store).use(router).mount('#app')
