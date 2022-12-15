bac5dfd3fe33659ae9f751cdb6924264c9fe4b69
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2022-12-15 09:57:32 +0200

    users ui

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -22,6 +22,50 @@
                 </div>
             </div>
         </div>
+        <div class="table_content">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titre 1</th>
+                        <th>Titre 1</th>
+                        <th>Titre 1</th>
+                        <th>Titre 1</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="spacer">
+                        <td colspan="100"></td>
+                    </tr>
+                    <tr>
+                        <td>dsfdsf</td>
+                        <td>dsfdsf</td>
+                        <td>dsfdsf</td>
+                        <td>dsfdsf</td>
+                    </tr>
+                    <tr class="spacer">
+                        <td colspan="100"></td>
+                    </tr>
+                    <tr>
+                        <td>dsfdsf</td>
+                        <td>dsfdsf</td>
+                        <td>dsfdsf</td>
+                        <td>dsfdsf</td>
+                    </tr>
+                    <tr class="spacer">
+                        <td colspan="10"></td>
+                    </tr>
+                    <tr>
+                        <td>dsfdsf</td>
+                        <td>dsfdsf</td>
+                        <td>dsfdsf</td>
+                        <td>dsfdsf</td>
+                    </tr>
+                    <tr class="spacer">
+                        <td colspan="100"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 <style src='../assets/css/users.css' scoped>
