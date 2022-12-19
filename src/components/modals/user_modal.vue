b6613cfbd0ada80b25e416aeb601d03e4c6b3299
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2022-12-19 12:05:07 +0200

    prepare user creation

--- src/components/modals/user_modal.vue
+++ src/components/modals/user_modal.vue
@@ -10,53 +10,92 @@
       <div class="inputs">
         <div class="part1">
                 <div class="input-container ic1">
-                    <input id="firstname" class="input" type="text" required placeholder=" " />
+                    <input id="firstname" v-model="form.name" class="input" type="text" required placeholder=" " />
                     <div class="cut"></div>
                     <label for="firstname" class="placeholder">Nom et Prenom</label>
                 </div>
                 <div class="input-container ic2">
-                    <input id="lastname" class="input" required type="text" placeholder=" " />
+                    <input id="lastname" v-model="form.email" class="input" required type="email" placeholder=" " />
                     <div class="cut"></div>
                     <label for="lastname" class="placeholder">Email</label>
                 </div>
                 <div class="input-container ic2">
-                    <input id="email" class="input" required type="email" placeholder=" " />
+                    <input id="email" v-model="form.telephone" class="input" required type="number" min="0" placeholder=" " />
                     <div class="cut"></div>
-                    <label for="email" class="placeholder">Telephone</label>
+                    <label for="email"  class="placeholder">Telephone</label>
                 </div>
             </div>
             <div class="part1">
                 <div class="input-container ic1">
-                    <select class="input" name="" id="">
+                    <select class="input" v-model="form.role" name="" id="">
                         <option value="">--Selectionner le Role--</option>
-                        <option value="">Admin</option>
-                        <option value="">Super Admin</option>
+                        <option value="Admin">Admin</option>
+                        <option value="Super Admin">Super Admin</option>
                     </select>
                     <div class="cut"></div>
                     <label for="firstname" class="placeholder">Role</label>
                 </div>
                 <div class="input-container ic2">
-                    <input id="lastname" class="input" type="password" placeholder=" " />
+                    <input id="lastname" v-model="form.password" class="input" type="password" placeholder=" " />
                     <div class="cut"></div>
                     <label for="lastname" required class="placeholder">Mot de passe</label>
                 </div>
                 <div class="input-container ic2">
-                    <input id="email" class="input" type="password" placeholder=" " />
+                    <input id="email" v-model="form.c_password" class="input" type="password" placeholder=" " />
                     <div class="cut"></div>
                     <label for="email" required class="placeholder">Confirmer mot de passe</label>
                 </div>
             </div>
          </div>
-      <button  class="submit">Ajouter</button>
+      <button  class="submit" @click="saveUser()">{{loading?"Chargement...":"Ajouter"}}</button>
     </div>
 </transition>
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
+    data(){
+        return{
+            form:{
+                name:'',
+                email:'',
+                telephone:'',
+                role:'',
+                password:'',
+                c_password:'',
+            },
+            errorMessage:"",
+            loading:false,
+        }
+    },
     methods:{
         close(){
             this.$emit('close')
+        },
+        saveUser(){
+            this.loading = true;
+            axios.post(this.url+'register',this.form)
+            .then((response)=>{
+                this.loading = false;
+                if(response.data.success){
+                    this.$store.commit("login",JSON.stringify(response.data.data))               
+                }
+                this.$toast.success(`Utilisateur enregistre`)  
+            })
+            .catch((error)=>{
+                if (error.message == "Network Error"){
+                    this.errorMessage = "Vous n'êtes pas connecté au serveur"
+                  
+                }else{
+                    this.errorMessage = error.response.data.message;
+                    this.loading = false;
+                    this.$toast.error(errorMessage,{
+                        position:"bottom-right"
+                    });
+                }
+                
+            })
         }
     },
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Toaster from "@meforma/vue-toaster";
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import AnimatedVue from 'animated-vue'
 
 
 
